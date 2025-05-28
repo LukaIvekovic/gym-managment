b41028ebbc494080d4d0377ebfa9e925e9dcb785
Luka <luka.ivekovic2@gmail.com>
2025-05-28 21:34:35 +0200

    Spring boot app initialized

---
+++ gym-managment/src/test/java/hr/fer/gymmanagment/GymManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.gymmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
